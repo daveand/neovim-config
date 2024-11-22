09e81b561279063729a6c9df37f71779f981867b
David <david.andersson@tuta.com>
2024-11-22 16:40:59 +0100

    updated config

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -3,4 +3,8 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 
+-- Set line numbers
+vim.opt.relativenumber = false;
+vim.opt.number = true;
+
 
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -2,6 +2,12 @@ vim.keymap.set('i', '<S-Tab>', '<End>')
 vim.keymap.set('n', '<C-d>', '<S-v>yp')
 vim.keymap.set('n', '<C-S-Down>', 'ddp')
 vim.keymap.set('n', '<C-S-Up>', 'ddkP')
+vim.keymap.set('n', '<C-s>', '<cmd>w<cr>')
+
+-- barbar
+vim.keymap.set('n', '<C-Left>', '<cmd>BufferPrevious<cr>')
+vim.keymap.set('n', '<C-Right>', '<cmd>BufferNext<cr>')
+vim.keymap.set('n', '<C-Del>', '<cmd>BufferClose<cr>')
 
 -- nvim-tree
 vim.keymap.set('n', '<C-n>', '<cmd>NvimTreeToggle<cr>')
